886b56ee0d15e2936ba8259481c19fd95a295217
Quinntyne Brown <quinntynebrown@gmail.com>
2016-10-28 16:13:03 -0400

    initial code commit

---
+++ src/vf.ts
@@ -0,0 +1,11 @@
+﻿import {
+    HeaderComponent,
+    HomePageComponent,
+
+    Router
+} from "./app";
+
+
+customElements.register("vf-router", Router);
+customElements.register("vf-header", HeaderComponent);
+customElements.register("vf-home-page", HomePageComponent);
